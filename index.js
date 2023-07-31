baf433fa33e600912c26876636612fc5709cf3a9
thesagarmahajan <sagarmahajan31997@gmail.com>
2023-07-31 20:10:04 +0530

    Capturing IP Address

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const express = require("express")
 app = express()
+
+const os = require('os');
+
+const getIpAddress = () => {
+  const interfaces = os.networkInterfaces();
+  let ipAddress = '';
+
+  // Iterate through network interfaces to find the IP address
+  for (const interfaceName in interfaces) {
+    const interfaceArray = interfaces[interfaceName];
+    for (const iface of interfaceArray) {
+      // Skip internal and IPv6 addresses
+      if (iface.internal || iface.family !== 'IPv4') continue;
+
+      ipAddress = iface.address;
+      break; // Stop iterating if we found the first non-internal IPv4 address
+    }
+
+    if (ipAddress) break; // Stop iterating if we found the IP address
+  }
+
+  return ipAddress;
+};
+
+// Usage example:
+const ipAddress = getIpAddress();
+
 const port = 80
 app.listen(port, ()=>{
     console.log(`Listening on port no: ${port}`)
@@ -11,33 +38,36 @@ app.get("/",(req,res)=>{
 
 // App Endpoints
 app.get("/app",(req,res)=>{
-    res.send("Welcome to App")
+    res.send(`<h1>Welcome to App FROM ${ipAddress}</h1>`)
 })
 
 app.get("/app/signup",(req,res)=>{
-    res.send("Welcome to Signup Page")
+    res.send(`<h1>Welcome to Signup Page FROM ${ipAddress}</h1>`)
 })
 
 app.get("/app/login",(req,res)=>{
-    res.send("Welcome to Login Page")
+    res.send(`<h1>Welcome to Login Page FROM ${ipAddress}</h1>`)
 })
 
 
 // API Endpnits
-app.post("/api",(req,res)=>{
-    res.send("Welcome to API")
+app.get("/api",(req,res)=>{
+    res.send(`<h1>Welcome to API FROM ${ipAddress}</h1>`)
 })
 
 app.post("/api/login",(req,res)=>{
-    res.send("You are requesting for Login API")
+    res.send(`<h1>You are requesting for Login API FROM ${ipAddress}</h1>`)
 })
 
 app.post("/api/Signup",(req,res)=>{
-    res.send("You are requesting for Signup API")
+    res.send(`<h1>You are requesting for Signup API FROM ${ipAddress}</h1>`)
 })
 
 app.get("/api/products",(req,res)=>{
-    res.json([
+    res.json({
+        ip:`${ipAddress}`,
+        payload:
+        [
         {
             id:1,
             name:"Product 1",
@@ -56,15 +86,15 @@ app.get("/api/products",(req,res)=>{
             price:300,
             description:"This is product 3"
         }
-    ])
+    ]})
 })
 
 
 // Logs Endpoints
 app.get("/logs",(req,res)=>{
-    res.send("Welcome to Logs")
+    res.send(`<h1>Welcome to Logs FROM ${ipAddress}</h1>`)
 })
 
 app.get("/logs/order/:orderid",(req,res)=>{
-    res.send("Fetching logs for latest order no. "+req.params.orderid)
+    res.send(`<h1>Fetching logs for latest order no. ${req.params.orderi} FROM ${ipAddress}</h1>`)
 })
